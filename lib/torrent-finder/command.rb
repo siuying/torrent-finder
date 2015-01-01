6047fd6bf76c5af5e4089763acb34709edfafd8d
Francis Chong <francis@ignition.hk>
2015-01-01 21:58:37 +0800

    add list command

--- lib/torrent-finder/command.rb
+++ lib/torrent-finder/command.rb
@@ -1,4 +1,7 @@
 require 'claide'
+require 'torrent-finder/adapters/popgo_adapter'
+require 'torrent-finder/adapters/eztv_adapter'
+require 'torrent-finder/adapters/nyaa_adapter'
 
 module TorrentFinder
   class Command < CLAide::Command
@@ -9,12 +12,14 @@ module TorrentFinder
     def self.options
       [
         ['--peerflix', 'launch peerflix with first matched result'],
-        ['--site=site', 'use site, default popgo']
+        ['--site=site', 'use site, default popgo'],
+        ['--list', 'list all available site']
       ].concat(super)
     end
 
     def initialize(argv)
       @use_peerflix = argv.flag?('peerflix', false)
+      @list = argv.flag?('list', false)
       @site = argv.option('site', "popgo")
       @keywords = argv.shift_argument
 
@@ -23,14 +28,20 @@ module TorrentFinder
 
     def run
       require "torrent-finder/adapters/#{@site}_adapter"
+
+      if @list
+        puts "Available Sites: " + TorrentFinder::Adapters::Registry.adapters.collect {|a| a.name }.join(", ")
+        return
+      end
+
       adapter_clazz = TorrentFinder::Adapters::Registry.adapters.find{|adapter| adapter.name == @site }
       adapter = adapter_clazz.new
-
       if @keywords
         torrents = adapter.search(@keywords)
       else
         torrents = adapter.list
       end
+
       if @use_peerflix
         torrent = torrents.find {|torrent| torrent.name.include?(@keywords) } || torrents.first
         exec %{peerflix #{torrent.url} --vlc}
